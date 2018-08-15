e2682d7a8cb559d269f02340eb2e2d9496801592
Samantha <Samantha15perez@gmail.com>
2018-08-14 21:16:26 -0400

    Base Project Finished

---
+++ Sql test/Sql test/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Sql_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.consumerInformationBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.perezDataSet1 = new Sql_test.PerezDataSet1();
+            this.consumerInformationTableAdapter = new Sql_test.PerezDataSet1TableAdapters.ConsumerInformationTableAdapter();
+            this.testQueryButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.consumerInformationBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.perezDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(448, 90);
+            this.textBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn,
+            this.phoneDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.consumerInformationBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 150);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(445, 100);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            // 
+            // phoneDataGridViewTextBoxColumn
+            // 
+            this.phoneDataGridViewTextBoxColumn.DataPropertyName = "Phone";
+            this.phoneDataGridViewTextBoxColumn.HeaderText = "Phone";
+            this.phoneDataGridViewTextBoxColumn.Name = "phoneDataGridViewTextBoxColumn";
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "Email";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            // 
+            // consumerInformationBindingSource
+            // 
+            this.consumerInformationBindingSource.DataMember = "ConsumerInformation";
+            this.consumerInformationBindingSource.DataSource = this.perezDataSet1;
+            // 
+            // perezDataSet1
+            // 
+            this.perezDataSet1.DataSetName = "PerezDataSet1";
+            this.perezDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // consumerInformationTableAdapter
+            // 
+            this.consumerInformationTableAdapter.ClearBeforeFill = true;
+            // 
+            // testQueryButton
+            // 
+            this.testQueryButton.Location = new System.Drawing.Point(201, 121);
+            this.testQueryButton.Name = "testQueryButton";
+            this.testQueryButton.Size = new System.Drawing.Size(75, 23);
+            this.testQueryButton.TabIndex = 2;
+            this.testQueryButton.Text = "Test Query";
+            this.testQueryButton.UseVisualStyleBackColor = true;
+            this.testQueryButton.Click += new System.EventHandler(this.testQueryButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.testQueryButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.consumerInformationBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.perezDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private PerezDataSet1 perezDataSet1;
+        private System.Windows.Forms.BindingSource consumerInformationBindingSource;
+        private PerezDataSet1TableAdapters.ConsumerInformationTableAdapter consumerInformationTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button testQueryButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Sql test/Sql test/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Sql_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = null;
+            SqlConnection cnn;
+            connectionString = "Data Source=PL13\\MTCDB;Initial Catalog=Perez; User ID = sperez; Password = sperez18";
+            cnn = new SqlConnection(connectionString);
+            try
+            {
+                cnn.Open();
+                label1.Text = "Connection Granted, Please enter a Query";
+                cnn.Close();
+            }
+            catch(Exception ex)
+            {
+                label1.Text = "Connection Denied, please contact Network Provider";
+            }
+        }
+
+        private void testQueryButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=PL13\\MTCDB;Initial Catalog=Perez; User ID = sperez; Password = sperez18";
+            SqlConnection cnn;
+            cnn = new SqlConnection(connectionString);
+            try
+            {
+
+
+                try
+                {
+
+                    DataTable dt = new DataTable();
+                    SqlCommand cmd = new SqlCommand();
+                    SqlDataAdapter adp = new SqlDataAdapter();
+
+                    cmd.Connection = cnn;
+                    adp.SelectCommand = cmd;
+                    cnn.Open();
+                    cmd.CommandText = textBox1.Text;
+                    adp.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("There was a problem parsing your query.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was a problem connecting to the server.");
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
